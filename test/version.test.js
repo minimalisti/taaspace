e7db0b06a19e2f5bc3734563bfd405a983275c58
Akseli Palen <akseli.palen@gmail.com>
2018-01-25 15:41:54 +0200

    Init gh-pages

--- test/version.test.js
+++
@@ -1,9 +0,0 @@
-var taaspace = require('../index')
-var semver = require('semver')
-
-module.exports = function (test) {
-  test('correctly formatted', function (t) {
-    t.ok(semver.valid(taaspace.version), 'valid semantic version')
-    t.end()
-  })
-}
